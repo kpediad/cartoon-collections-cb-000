58e840e65fcdbc90bb2fb6899c37a2ddd48b966f
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-20 10:51:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find {|ingredient| cheese_types.include?(ingredient)}
 end
